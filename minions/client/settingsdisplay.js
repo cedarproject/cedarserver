0e774b56a0df62c8b8d735c8e6d1bf6626d77f64
Isaac Smith <isaac@isrv.pw>
2015-04-11 21:30:46 -0500

    Added post-box setting

--- minions/client/settingsdisplay.js
+++ minions/client/settingsdisplay.js
@@ -18,6 +18,7 @@ Template.minionsettingsdisplay.events({
     'click .add-block': function (event) {
         var blocks = this.settings.blocks;
         blocks.push({left: 0, right: 0, top: 0, bottom: 0,
+                     'post-left': 0, 'post-right': 0, 'post-top': 0, 'post-bottom': 0,
                      'translation-x': 0, 'translation-y': 0, 'translation-z': 0,
                      'rotation-x': 0, 'rotation-y': 0, 'rotation-z': 0,
                      'scale-x': 1, 'scale-y': 1,
@@ -42,6 +43,10 @@ Template.minionsettingsdisplay.events({
             right: parseInt(row.find('.disp-right').val()) || 0,
             top: parseInt(row.find('.disp-top').val()) || 0,
             bottom: parseInt(row.find('.disp-bottom').val()) || 0,
+            'post-left': parseInt(row.find('.disp-post-left').val()) || 0,
+            'post-right': parseInt(row.find('.disp-post-right').val()) || 0,
+            'post-top': parseInt(row.find('.disp-post-top').val()) || 0,
+            'post-bottom': parseInt(row.find('.disp-post-bottom').val()) || 0,
             'translation-x': parseFloat(row.find('.disp-trans-x').val()) || 0,
             'translation-y': parseFloat(row.find('.disp-trans-y').val()) || 0,
             'translation-z': parseFloat(row.find('.disp-trans-z').val()) || 0,
