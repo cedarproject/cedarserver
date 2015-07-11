719971587e7695c3af3ff1f30044bf3f03899b26
Isaac Smith <isaac@isrv.pw>
2015-07-11 14:24:18 -0500

    Added scene clone feature

--- lighting/client/scenes.js
+++ lighting/client/scenes.js
@@ -7,5 +7,9 @@ Template.lightScenes.helpers({
 Template.lightScenes.events({
     'click .scene-add': function (event) {
         Meteor.call('sceneAdd');
+    },
+    
+    'click .scene-clone': function (event, template) {
+        Meteor.call('sceneClone', this._id);
     }
 });
