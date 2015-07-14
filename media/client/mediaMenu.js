923e3823efcd1470360d1b0232a99a7fac4c085e
Isaac Smith <isaac@isrv.pw>
2015-07-14 14:45:03 -0500

    Added file I forgot last commit

---
+++ media/client/mediaMenu.js
@@ -0,0 +1,9 @@
+Template.mediaMenu.helpers({
+    mediaSelector: {
+        collection: media,
+        displayTemplate: 'media',
+        fields: [{field: 'title', type: String}, {field: 'tags', type: Array}],
+        sort: [['new', 1], ['title', 1]],
+        addbutton: false
+    }
+});
